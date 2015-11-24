3158bd8c6eaf17be5acf023015a2c256c630db55
bcemmett <bcemmett@users.noreply.github.com>
2015-11-24 13:17:10 +0000

    Include RecompileDbCommandInterceptor

---
+++ EntityFrameworkSchoolSystem/RecompileDbCommandInterceptor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.Entity.Infrastructure.Interception;
+
+namespace EntityFrameworkSchoolSystem
+{
+    public class RecompileDbCommandInterceptor : IDbCommandInterceptor
+    {
+        public void ReaderExecuting(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+            if (!command.CommandText.EndsWith(" option(recompile)"))
+            {
+                command.CommandText += " option(recompile)";
+            }
+        }
+
+        public void NonQueryExecuting(DbCommand command, DbCommandInterceptionContext<Int32> interceptionContext)
+        {
+            if (!command.CommandText.EndsWith(" option(recompile)"))
+            {
+                command.CommandText += " option(recompile)";
+            }
+        }
+
+        public void ScalarExecuting(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            if (!command.CommandText.EndsWith(" option(recompile)"))
+            {
+                command.CommandText += " option(recompile)";
+            }
+        }
+
+        public void ReaderExecuted(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+        }
+
+        public void NonQueryExecuted(DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+        }
+
+        public void ScalarExecuted(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+        }
+    }
+}
